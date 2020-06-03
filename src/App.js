eb3f64d22ea5a613d97cf38e34ddbe03eb0d7afc
Youtop1a <oleeof@gmail.com>
2020-06-03 15:47:19 +0300

    Add card changing mode

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import Header from "./components/Header";
 import Card from "./components/Card";
 
 function App() {
+  const [cardsData, setCardsData] = React.useState([
+    {
+      id: 2314,
+      header: "Great News",
+      text:
+        "Импликация, следовательно, контролирует бабувизм, открывая новые горизонты.",
+    },
+  ]);
+  const changeCard = (id, newData) => {
+    const cards = cardsData.map((card) => {
+      if (card.id === id) {
+        card.header = newData.header;
+        card.text = newData.text;
+      }
+      return card;
+    });
+    setCardsData(cards);
+  };
+
   return (
     <div>
       <Header></Header>
       <div style={{ paddingTop: "10px", paddingLeft: "10px" }}>
         <Card
-          header="Great News"
-          text="Импликация, следовательно, контролирует бабувизм, открывая новые горизонты."
+          id={cardsData[0].id}
+          header={cardsData[0].header}
+          text={cardsData[0].text}
+          changeCard={changeCard}
         />
       </div>
     </div>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { MdModeEdit, MdDone, MdClose } from "react-icons/md";
 
 const Card = (props) => {
   const [isChecked, setChecked] = React.useState(false);
+  const [isChange, setChange] = React.useState(false);
+  const [values, setValues] = React.useState({
+    header: props.header,
+    text: props.text,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
   const styles = {
     header: {
       margin: "5px",
@@ -25,21 +36,73 @@ const Card = (props) => {
     textChecked: "blue",
     textDefault: "black",
   };
+
+  const butBlock = isChange ? (
+    <React.Fragment>
+      <button
+        onClick={() => {
+          props.changeCard(props.id, values);
+          setChange(false);
+        }}
+      >
+        <MdDone />
+      </button>
+      <button
+        onClick={() => {
+          setValues({ header: props.header, text: props.text });
+          setChange(false);
+        }}
+      >
+        <MdClose />
+      </button>
+    </React.Fragment>
+  ) : (
+    <React.Fragment>
+      <button
+        onClick={() => {
+          setChecked(false);
+          setChange(true);
+        }}
+      >
+        <MdModeEdit />
+      </button>
+      <input type="checkbox" onChange={() => setChecked(!isChecked)} />
+    </React.Fragment>
+  );
+
   return (
     <div style={styles.block}>
       <div style={styles.flex}>
-        <div style={styles.header}>{props.header}</div>
-        <input type="checkbox" onChange={() => setChecked(!isChecked)} />
+        {isChange ? (
+          <input
+            type="text"
+            value={values.header}
+            onChange={handleChange("header")}
+            style={{ margin: "5px" }}
+          />
+        ) : (
+          <div style={styles.header}>{props.header}</div>
+        )}
+        <div style={{ ...styles.flex, padding: "0px" }}>{butBlock}</div>
       </div>
       <hr />
-      <div
-        style={{
-          ...styles.text,
-          color: isChecked ? styles.textChecked : styles.textDefault,
-        }}
-      >
-        {props.text}
-      </div>
+      {isChange ? (
+        <textarea
+          style={{ ...styles.text, resize: "none", width: "90%" }}
+          rows="10"
+          onChange={handleChange("text")}
+          value={values.text}
+        ></textarea>
+      ) : (
+        <div
+          style={{
+            ...styles.text,
+            color: isChecked ? styles.textChecked : styles.textDefault,
+          }}
+        >
+          {props.text}
+        </div>
+      )}
     </div>
   );
 };
