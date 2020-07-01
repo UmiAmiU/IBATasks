0dc505885b9c79e57ecc68e3489254d09f1f68d3
Youtop1a <oleeof@gmail.com>
2020-07-01 23:28:40 +0300

    Add router and auth page/component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Auth from "./components/Auth";
 import { CardContextProvider } from "./components/context/card-context.jsx";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <CardContextProvider>
-        <Header></Header>
-        <Main></Main>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route path="/" exact component={Main} />
+            <Route path="/auth" exact component={Auth} />
+          </Switch>
+        </Router>
       </CardContextProvider>
     </div>
   );
